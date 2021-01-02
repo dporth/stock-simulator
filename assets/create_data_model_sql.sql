9058d2fb8578c1de25ed91ce5ca5c218497991fd
Desmond Porth <desaporth@gmail.com>
2021-01-02 10:15:40 -0700

    Created data model ddl sql

---
+++ assets/create_data_model_sql.sql
@@ -0,0 +1,149 @@
+USE [PERSONAL]
+GO
+/****** Object:  Table [stockmarket].[account]    Script Date: 1/2/2021 10:14:57 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [stockmarket].[account](
+	[account_id] [int] IDENTITY(1,1) NOT NULL,
+	[usd_amount] [numeric](38, 6) NULL,
+	[share_amount] [numeric](38, 6) NULL,
+	[user_id] [int] NULL,
+	[stock_id] [int] NULL,
+	[account_value_id] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[account_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [stockmarket].[account_value]    Script Date: 1/2/2021 10:14:57 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [stockmarket].[account_value](
+	[account_value_id] [int] IDENTITY(1,1) NOT NULL,
+	[valid_from] [datetime] NULL,
+	[valid_to] [datetime] NULL,
+	[usd_account_amount] [numeric](38, 6) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[account_value_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [stockmarket].[address]    Script Date: 1/2/2021 10:14:57 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [stockmarket].[address](
+	[address_id] [int] IDENTITY(1,1) NOT NULL,
+	[street] [nvarchar](1000) NULL,
+	[postal_code] [nvarchar](50) NULL,
+	[city_id] [int] NULL,
+	[state_id] [int] NULL,
+	[country_id] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[address_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [stockmarket].[city]    Script Date: 1/2/2021 10:14:57 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [stockmarket].[city](
+	[city_id] [int] IDENTITY(1,1) NOT NULL,
+	[city_name] [nvarchar](256) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[city_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [stockmarket].[country]    Script Date: 1/2/2021 10:14:57 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [stockmarket].[country](
+	[country_id] [int] IDENTITY(1,1) NOT NULL,
+	[country_name] [nvarchar](256) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[country_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [stockmarket].[state]    Script Date: 1/2/2021 10:14:57 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [stockmarket].[state](
+	[state_id] [int] IDENTITY(1,1) NOT NULL,
+	[state_name] [nvarchar](256) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[state_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [stockmarket].[stock]    Script Date: 1/2/2021 10:14:57 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [stockmarket].[stock](
+	[stock_id] [int] IDENTITY(1,1) NOT NULL,
+	[symbol] [nvarchar](50) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[stock_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [stockmarket].[user]    Script Date: 1/2/2021 10:14:57 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [stockmarket].[user](
+	[user_id] [int] IDENTITY(1,1) NOT NULL,
+	[first_name] [nvarchar](1000) NULL,
+	[last_name] [nvarchar](50) NULL,
+	[address_id] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[user_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [stockmarket].[account_value] ADD  DEFAULT (getdate()) FOR [valid_from]
+GO
+ALTER TABLE [stockmarket].[account]  WITH CHECK ADD FOREIGN KEY([account_value_id])
+REFERENCES [stockmarket].[account_value] ([account_value_id])
+GO
+ALTER TABLE [stockmarket].[account]  WITH CHECK ADD FOREIGN KEY([stock_id])
+REFERENCES [stockmarket].[stock] ([stock_id])
+GO
+ALTER TABLE [stockmarket].[account]  WITH CHECK ADD FOREIGN KEY([user_id])
+REFERENCES [stockmarket].[user] ([user_id])
+GO
+ALTER TABLE [stockmarket].[address]  WITH CHECK ADD FOREIGN KEY([city_id])
+REFERENCES [stockmarket].[city] ([city_id])
+GO
+ALTER TABLE [stockmarket].[address]  WITH CHECK ADD FOREIGN KEY([country_id])
+REFERENCES [stockmarket].[country] ([country_id])
+GO
+ALTER TABLE [stockmarket].[address]  WITH CHECK ADD FOREIGN KEY([state_id])
+REFERENCES [stockmarket].[state] ([state_id])
+GO
+ALTER TABLE [stockmarket].[user]  WITH CHECK ADD FOREIGN KEY([address_id])
+REFERENCES [stockmarket].[address] ([address_id])
+GO
