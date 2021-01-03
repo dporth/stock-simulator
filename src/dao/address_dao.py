3d062ae24d442d2badcc6ec3c376f112bcede704
Desmond Porth <desaporth@gmail.com>
2021-01-02 18:48:00 -0700

    Work on dao implementations, finished address doa

---
+++ src/dao/address_dao.py
@@ -0,0 +1,55 @@
+import sys
+[sys.path.append(i) for i in ['.', '..','../../', '../db/']]
+from src.db.db_helper import DBHelper
+from src.db.models import Address
+
+
+class AddressDAO():
+
+    def __init__(self):
+        self._db = DBHelper()
+
+    def get_address(self):
+        """Returns all addresss."""
+        with self._db.session_scope() as session:
+            return session.query(Address)
+
+    def delete_address(self, address_id):
+        """Deletes all records in the address table that have address id specified. Returns the address id of all records deleted."""
+        with self._db.session_scope() as session:
+            marked_to_delete = []
+            results = session.query(Address.address_id).filter_by(address_id=address_id)
+            for row in results:
+                marked_to_delete.append(row[0])
+            address = session.query(Address).filter_by(address_id=address_id).delete()
+            session.flush()
+            return marked_to_delete
+
+    def create_address(self, street, postal_code, country_id, state_id, city_id):
+        """Creates a record in the address table with the parameters specified. Returns the address id of the record created."""
+        with self._db.session_scope() as session:
+            address = Address(street=street, postal_code=postal_code, country_id=country_id, state_id=state_id, city_id=city_id)
+            session.add(address)
+            session.flush()
+            return address.address_id
+
+if __name__ == "__main__":
+    address = AddressDAO()
+
+    # Insert address functionality
+    street = '6th way place'
+    postal_code = '123456'
+    country_id = '2'
+    state_id = '2'
+    city_id = '2'
+
+
+    id = address.create_address(street, postal_code, country_id, state_id, city_id)
+    print(id)
+    # Get address functionality
+    results = address.get_address()
+    for row in results:
+        print(row)
+
+    # Delete address funcitonality
+    print(address.delete_address(id))
--- src/db/models.py
+++ src/db/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 import sys
 [sys.path.append(i) for i in ['.', '..','../../']]
 from .db_helper import DBHelper
@@ -22,17 +23,16 @@ class User(Base):
     user_id = Column(Integer, primary_key=True)
     first_name = Column(String)
     last_name = Column(String)
-    #implement
-    #address_id = Column(Integer, ForeignKey('parent.id'))
 
     def __repr__(self):
-        return "<User(user_id='%s', first_name='%s', last_name='%s')>" % (self.user_id, self.first_name, self.last_name)
+        return "<User(user_id='%s', first_name='%s', last_name='%s', address_id='%s')>" % (self.user_id, self.first_name, self.last_name, self.address_id)
 
 class City(Base):
     __tablename__ = 'city'
     __table_args__ = {'schema' : _db._schema}
     city_id = Column(Integer, primary_key=True)
     city_name = Column(String)
+    children = relationship("Address")
 
     def __repr__(self):
         return "<City(city_id='%s', city_name='%s')>" % (self.city_id, self.city_name)
@@ -42,6 +42,7 @@ class Country(Base):
     __table_args__ = {'schema' : _db._schema}
     country_id = Column(Integer, primary_key=True)
     country_name = Column(String)
+    children = relationship("Address")
 
     def __repr__(self):
         return "<Country(country_id='%s', country_name='%s')>" % (self.country_id, self.country_name)
@@ -51,6 +52,20 @@ class State(Base):
     __table_args__ = {'schema' : _db._schema}
     state_id = Column(Integer, primary_key=True)
     state_name = Column(String)
-
+    children = relationship("Address")
+    
     def __repr__(self):
         return "<State(state_id='%s', state_name='%s')>" % (self.state_id, self.state_name)
+
+class Address(Base):
+    __tablename__ = 'address'
+    __table_args__ = {'schema' : _db._schema}
+    address_id = Column(Integer, primary_key=True)
+    street = Column(String)
+    postal_code = Column(String)
+    state_id = Column(Integer, ForeignKey(f'{_db._schema}.state.state_id'))
+    country_id = Column(Integer, ForeignKey(f'{_db._schema}.country.country_id'))
+    city_id = Column(Integer, ForeignKey(f'{_db._schema}.city.city_id'))
+
+    def __repr__(self):
+        return "<Address(address_id='%s', street='%s', postal_code='%s', state_id='%s', country_id='%s', city_id='%s')>" % (self.address_id, self.street, self.postal_code, self.state_id, self.country_id, self.city_id)
