6b81b24e1799903ae9d01f92b8aeeec335d45bb8
Desmond Porth <desaporth@gmail.com>
2021-03-20 12:27:59 -0600

    Removing access files

--- assets/create_data_model_sql.sql
+++ assets/create_data_model_sql.sql
@@ -1,4 +1,6 @@
-/****** Object:  Table [stockmarket].[account]    Script Date: 2/13/2021 5:38:48 PM ******/
+USE [STS]
+GO
+/****** Object:  Table [stockmarket].[account]    Script Date: 3/20/2021 12:21:43 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -15,7 +17,7 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [stockmarket].[account_value]    Script Date: 2/13/2021 5:38:48 PM ******/
+/****** Object:  Table [stockmarket].[account_value]    Script Date: 3/20/2021 12:21:44 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -32,29 +34,52 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [stockmarket].[location]    Script Date: 2/13/2021 5:38:48 PM ******/
+/****** Object:  Table [stockmarket].[stock]    Script Date: 3/20/2021 12:21:44 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [stockmarket].[location](
-	[location_id] [int] IDENTITY(1,1) NOT NULL,
-	[state_id] [int] NULL,
-	[country_id] [int] NULL,
+CREATE TABLE [stockmarket].[stock](
+	[stock_id] [int] IDENTITY(1,1) NOT NULL,
+	[symbol] [nvarchar](50) NOT NULL,
 PRIMARY KEY CLUSTERED 
 (
-	[location_id] ASC
+	[stock_id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [stockmarket].[user]    Script Date: 2/13/2021 5:38:48 PM ******/
+/****** Object:  Table [stockmarket].[stock_price_history]    Script Date: 3/20/2021 12:21:44 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [stockmarket].[stock_price_history](
+	[stock_price_history_id] [int] IDENTITY(1,1) NOT NULL,
+	[stock_id] [int] NOT NULL,
+	[historical_usd_price] [numeric](38, 6) NOT NULL,
+	[valid_from] [datetime] NULL,
+	[valid_to] [datetime] NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [stockmarket].[stock_price_queue]    Script Date: 3/20/2021 12:21:44 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [stockmarket].[stock_price_queue](
+	[queue_id] [int] IDENTITY(1,1) NOT NULL,
+	[stock_id] [int] NOT NULL,
+	[etl_date] [datetime] NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [stockmarket].[user]    Script Date: 3/20/2021 12:21:44 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [stockmarket].[user](
 	[user_id] [nvarchar](256) NOT NULL,
-	[email] [nvarchar](320) NULL
+	[identifier] [nvarchar](320) NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[user_id] ASC
@@ -63,21 +88,22 @@ PRIMARY KEY CLUSTERED
 GO
 ALTER TABLE [stockmarket].[account_value] ADD  DEFAULT (getdate()) FOR [valid_from]
 GO
+ALTER TABLE [stockmarket].[stock_price_history] ADD  DEFAULT (getdate()) FOR [valid_from]
+GO
+ALTER TABLE [stockmarket].[stock_price_queue] ADD  DEFAULT (getdate()) FOR [etl_date]
+GO
 ALTER TABLE [stockmarket].[account]  WITH CHECK ADD FOREIGN KEY([stock_id])
 REFERENCES [stockmarket].[stock] ([stock_id])
+GO
+ALTER TABLE [stockmarket].[account]  WITH CHECK ADD FOREIGN KEY([user_id])
+REFERENCES [stockmarket].[user] ([user_id])
 GO
 ALTER TABLE [stockmarket].[account_value]  WITH CHECK ADD FOREIGN KEY([account_id])
 REFERENCES [stockmarket].[account] ([account_id])
 GO
-ALTER TABLE [stockmarket].[location]  WITH CHECK ADD FOREIGN KEY([country_id])
-REFERENCES [stockmarket].[country] ([country_id])
+ALTER TABLE [stockmarket].[stock_price_history]  WITH CHECK ADD FOREIGN KEY([stock_id])
+REFERENCES [stockmarket].[stock] ([stock_id])
 GO
-ALTER TABLE [stockmarket].[location]  WITH CHECK ADD FOREIGN KEY([country_id])
-REFERENCES [stockmarket].[country] ([country_id])
-GO
-ALTER TABLE [stockmarket].[location]  WITH CHECK ADD FOREIGN KEY([state_id])
-REFERENCES [stockmarket].[state] ([state_id])
-GO
-ALTER TABLE [stockmarket].[location]  WITH CHECK ADD FOREIGN KEY([state_id])
-REFERENCES [stockmarket].[state] ([state_id])
+ALTER TABLE [stockmarket].[stock_price_queue]  WITH CHECK ADD FOREIGN KEY([stock_id])
+REFERENCES [stockmarket].[stock] ([stock_id])
 GO
